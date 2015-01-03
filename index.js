6a192a58ca7adfce2332faa5e4ad24ae17457b75
osk <oskosk@gmail.com>
2015-01-03 13:40:30 -0300

    -baseUrl is now the URL of a geoserver instance, not its wms endpoint

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+var debug = require("debug"),
+  urljoin = require("url-join");
 module.exports = geoserver;
 
 function geoserver(baseUrl) {
   if (!(this instanceof geoserver)) {
     return new geoserver(baseUrl);
   }
-  this.wms = require("./lib/wms")(baseUrl);
+  if (!baseUrl) {
+    throw (new Error("A baseUrl for a running geoserver instance is mandatory"));
+  }
+  debug("geoserver-client instance created for %s", baseUrl);
+  this.wms = require("./lib/wms")(urljoin(baseUrl, "wms"));
 }
