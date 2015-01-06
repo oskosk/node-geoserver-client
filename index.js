4dab865200908cd95d846a4ecbb4f44e29f326a5
osk <oskosk@gmail.com>
2015-01-06 09:36:22 -0300

    now wms methods, depend on module `wms-client`

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var debug = require("debug"),
+  wmsclient = require("wms-client"),
   urljoin = require("url-join");
 module.exports = geoserver;
 
-function geoserver(baseUrl) {
+function geoserver(baseUrl, options) {
   if (!(this instanceof geoserver)) {
-    return new geoserver(baseUrl);
+    return new geoserver(baseUrl, options);
   }
   if (!baseUrl) {
     throw (new Error("A baseUrl for a running geoserver instance is mandatory"));
   }
   debug("geoserver-client instance created for %s", baseUrl);
-  this.wms = require("./lib/wms")(urljoin(baseUrl, "wms"));
+  this.wms = wmsclient(urljoin(baseUrl, "wms"));
+  this.admin = require("./lib/admin")(urljoin(baseUrl, "rest"), options, {});
 }
